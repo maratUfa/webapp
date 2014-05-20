2f5f8fd34c4ed9745407a664857f2fe36a591795
Marat Shaekhmetov <marat.ufa@gmail.com>
2014-05-20 17:30:46 +0600

    moved to tomcat

--- src/main/java/controllers/MainController.java
+++ src/main/java/controllers/MainController.java
@@ -1,28 +1,22 @@
 package controllers;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestMethod;
 
-/**
- * Created by Home on 14.04.2014.
- */
-@ComponentScan
 @Controller
-@EnableAutoConfiguration
+@ComponentScan
 public class MainController {
 
-    @RequestMapping("/")
-    public String index(){
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+
+    public String index(ModelMap model){
+        model.put("message", "test message");
         return "index";
     }
 
-    public static void main(String[] args){
-        SpringApplication.run(MainController.class, args);
-    }
 
 
 }
